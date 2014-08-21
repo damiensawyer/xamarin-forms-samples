33925b8d5ad3c76d5961a8ba60d2133e65a89af4
Damien Sawyer <damiensawyer@gmail.com>
2014-08-21 22:27:40 +1000

    mucking around

--- XAMLSamples/xamlsamples/xamlsamples/AbsoluteDemoPage.xaml.cs
+++ XAMLSamples/xamlsamples/xamlsamples/AbsoluteDemoPage.xaml.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace XamlSamples
 {
-    public partial class AbsoluteDemoPage
-    {
-        public AbsoluteDemoPage()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class AbsoluteDemoPage
+	{
+		public AbsoluteDemoPage ()
+		{
+			InitializeComponent ();
+			myLayout.Children.Add (new BoxView { Color = Color.Green }, new Rectangle (0, 0, 0.3, 0.3), AbsoluteLayoutFlags.All);
+		}
+
+		void OnSliderValueChanged (object sender, 
+		                           ValueChangedEventArgs args)
+		{
+			var x = args.NewValue;
+			var sss = new AbsoluteLayout ();
+			//this.myBox.Layout (new Xamarin.Forms.Rectangle (x, 0, 0.25, 0.25));
+			
+		}
+	}
 }
--- XAMLSamples/xamlsamples/xamlsamples/XamlPlusCodePage.xaml.cs
+++ XAMLSamples/xamlsamples/xamlsamples/XamlPlusCodePage.xaml.cs
@@ -3,25 +3,26 @@ using Xamarin.Forms;
 
 namespace XamlSamples
 {
-    public partial class XamlPlusCodePage
-    {
-        public XamlPlusCodePage()
-        {
-            InitializeComponent();
-        }
+	public partial class XamlPlusCodePage
+	{
+		public XamlPlusCodePage ()
+		{
+			InitializeComponent ();
 
-        void OnSliderValueChanged(object sender, 
-                                  ValueChangedEventArgs args)
-        {
-            valueLabel.Text = args.NewValue.ToString("F3");
-        }
+		}
 
-        async void OnButtonClicked(object sender, EventArgs args)
-        {
-            Button button = (Button)sender;
-            await DisplayAlert("Clicked!",
-                "The button labeled '" + button.Text + "' has been clicked",
-                "OK", null);
-        }
-    }
+		void OnSliderValueChanged (object sender, 
+		                           ValueChangedEventArgs args)
+		{
+			valueLabel.Text = args.NewValue.ToString ("F3");
+		}
+
+		async void OnButtonClicked (object sender, EventArgs args)
+		{
+			Button button = (Button)sender;
+			await DisplayAlert ("Clicked!",
+				"The button labeled '" + button.Text + "' has been clicked",
+				"OK", null);
+		}
+	}
 }
